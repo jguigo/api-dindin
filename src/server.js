000ebd8b005b9f3ed9564afb879329375a498b88
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-04-19 00:33:57 -0300

    correção de erro

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const routes = require("./routes");
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
-app.use(routes());
+app.use(routes);
 
 app.listen(4000, () => console.log("Servidor on na porta 4000"));
