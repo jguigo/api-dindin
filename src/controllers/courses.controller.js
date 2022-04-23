777cf57a7023a4e6ca3bbe4e474d3a2bdf7f691c
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-04-22 21:21:02 -0300

    feat: create course

--- src/controllers/courses.controller.js
+++ src/controllers/courses.controller.js
@@ -1,6 +1,19 @@
-const Courses = require("..models/Courses.js");
-
+const Courses = require("../models/Courses");
 
 const CoursesController = {
-   
-}
+   async newCourse(req, res) {
+      try {
+         const { title, description, teacher } = req.body;
+         let createCourse = await Courses.create({
+            title,
+            description,
+            teacher,
+         });
+         res.json(createCourse);
+      } catch (error) {
+         console.log(error);
+      }
+   },
+};
+
+module.exports = CoursesController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const CoursesController = require('../controllers/courses.controller');
 const routes = express.Router();
 
-
+routes.post('/cursos', CoursesController.newCourse)
 
 module.exports = routes;
